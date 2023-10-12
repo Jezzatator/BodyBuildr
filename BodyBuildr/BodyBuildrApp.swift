6bf97a5e5c72e0bd170d10d896b0c3e0545ad130
Jezzatator <jeremie.patot@protonmail.ch>
2023-10-12 15:02:16 +0200

    creation du projet initial - iOS 17

---
+++ BodyBuildr/BodyBuildrApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyBuildrApp.swift
+//  BodyBuildr
+//
+//  Created by Jérémie - Ada on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BodyBuildrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BodyBuildr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BodyBuildr
+//
+//  Created by Jérémie - Ada on 12/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
